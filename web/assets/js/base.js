498f27e5d4355dd4311e02172a123f4a61750e1a
Bogdan Dinu <bogdan.dinu@emag.ro>
2019-07-12 16:27:19 +0300

    add functionality for sonde

--- web/assets/js/base.js
+++ web/assets/js/base.js
@@ -9,17 +9,30 @@ var SONDE = {
             type: 'POST',
             data: {'sector': sector},
             success: function (data) {
-                var sondeList = $("#sonde-list");
+                var $sondeList = $("#sonde-list");
+                var $sectorId = "id_sector_" + sector;
+                var $sectors = $(".sectors");
+                var $className = "";
                 if (data !== null && data.hasOwnProperty("sonde")) {
-                    sondeList.parent().show();
-                    sondeList.empty();
+                    $sondeList.parent().show();
+                    $sondeList.empty();
                     $.each(data.sonde, function(k, v) {
-                        sondeList.append("<li class='list-group-item'>"
+                        $sondeList.append("<li class='list-group-item'>"
                             + "<a href='/sonde_details/" + v.id + "'>" + v.name
                             + "</a>"
                             + "</li>"
                         );
                     });
+                    $sectors.removeClass().addClass("sectors");
+                    $sectors.each(function(){
+                        var idElement = $(this).attr('id');
+                        if (idElement === $sectorId) {
+                            $className = "btn btn-danger";
+                        } else {
+                            $className = "btn btn-secondary";
+                        }
+                        $(this).addClass($className);
+                    })
                 } else {
                     alert(data.message);
                 }
